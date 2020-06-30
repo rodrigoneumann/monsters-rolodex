d4ea48012322b893b7bf2857b09b35e39709c685
Rodrigo Neumann de Souza <rneumann@MacBook-Pro-de-Rodrigo.local>
2020-06-30 12:29:21 +0100

    Updates

---
+++ precache-manifest.71ee6327ac2b5b9e35292371c0996bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343920f886e504293211237d308b7871",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9b960572a16e27879d26",
+    "url": "/monsters-rolodex/static/css/main.3f3fb0e3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b960572a16e27879d26",
+    "url": "/monsters-rolodex/static/js/main.57d33f46.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
